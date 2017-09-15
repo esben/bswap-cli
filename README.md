1b720a6724ff82e2878472f5c457c0599c99c23a
Esben Haabendal <esben@haabendal.dk>
2017-09-15 10:11:00 +0200

    Initial code drop
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # bswap-cli
-Simple CLI wrapper for bswap(3) functions
+
+This project provides a simple CLI wrapper for the 3 bswap(3) functions:
+bswap\_16(), bswap\_32(), and bswap\_64(), allowing for reversing the byte
+order of files.
+
+The output is zero padded if needed in order to reverse the final bytes.
+
+## Usage
+
+```sh
+$ hexdump -C test.bin
+00000000  41 42 43 44 45 46 47 48  49 4a 4b                 |ABCDEFGHIJK|
+0000000b
+$ bswap-16 test.bin test.bin~16
+$ hexdump -C test.bin~16
+00000000  42 41 44 43 46 45 48 47  4a 49 00 4b              |BADCFEHGJI.K|
+0000000c
+$ bswap-32 test.bin test.bin~32
+$ hexdump -C test.bin~32
+00000000  44 43 42 41 48 47 46 45  00 4b 4a 49              |DCBAHGFE.KJI|
+0000000c
+$ bswap-64 test.bin test.bin~64
+$ hexdump -C test.bin~64
+00000000  48 47 46 45 44 43 42 41  00 00 00 00 00 4b 4a 49  |HGFEDCBA.....KJI|
+00000010
+```
---
+++ src/bswap.c
@@ -0,0 +1,135 @@
+/*
+ * Simple wrapper to bswap(3) C functions
+ *
+ * Copyright 2017 Esben Haabendal <esben@haabendal.dk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <byteswap.h>
+
+static void usage(const char *progname)
+{
+	printf("usage: %s [-h] [INPUT] [OUTPUT]\n", progname);
+}
+
+static void read_bswap_write_16(FILE *in, FILE *out)
+{
+	uint16_t buf;
+	size_t read;
+
+	read = fread(&buf, 1, sizeof(buf), in);
+	if (read == 0)
+		return;
+	if (read < sizeof(buf))
+		buf &= ((1 << (read * 8)) - 1);
+	buf = bswap_16(buf);
+	fwrite(&buf, 1, sizeof(buf), out);
+}
+
+static void read_bswap_write_32(FILE *in, FILE *out)
+{
+	uint32_t buf;
+	size_t read;
+
+	read = fread(&buf, 1, sizeof(buf), in);
+	if (read == 0)
+		return;
+	if (read < sizeof(buf))
+		buf &= ((1 << (read * 8)) - 1);
+	buf = bswap_32(buf);
+	fwrite(&buf, 1, sizeof(buf), out);
+}
+
+static void read_bswap_write_64(FILE *in, FILE *out)
+{
+	uint64_t buf;
+	size_t read;
+
+	read = fread(&buf, 1, sizeof(buf), in);
+	if (read == 0)
+		return;
+	if (read < sizeof(buf))
+		buf &= ((1 << (read * 8)) - 1);
+	buf = bswap_64(buf);
+	fwrite(&buf, 1, sizeof(buf), out);
+}
+
+int main(int argc, char **argv)
+{
+	char *progname;
+	void (*read_bswap_write)(FILE *in, FILE *out);
+	int opt;
+	FILE *in = stdin, *out = stdout;
+
+	progname = basename(argv[0]);
+	if (strcmp(progname, "bswap-16") == 0) {
+		read_bswap_write = read_bswap_write_16;
+	} else if (strcmp(progname, "bswap-32") == 0) {
+		read_bswap_write = read_bswap_write_32;
+	} else if (strcmp(progname, "bswap-64") == 0) {
+		read_bswap_write = read_bswap_write_64;
+	} else {
+		fprintf(stderr, "Error: invalid program name: %s\n", progname);
+		printf("Please install as one of: "
+		       "bswap-16, bswap-32 or bswap-64\n");
+		return EXIT_FAILURE;
+	}
+
+	while ((opt = getopt(argc, argv, "h")) != -1) {
+		switch (opt) {
+		case 'h':
+			usage(progname);
+			return EXIT_SUCCESS;
+		default:
+			usage(progname);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (argc >= 2) {
+		in = fopen(argv[1], "rb");
+		if (in == NULL) {
+			perror("fopen");
+			return EXIT_FAILURE;
+		}
+	}
+
+	if (argc >= 3) {
+		out = fopen(argv[2], "wb");
+		if (out == NULL) {
+			perror("fopen");
+			return EXIT_FAILURE;
+		}
+	}
+ 
+	while (1) {
+		if (feof(in))
+			break;
+		if (ferror(in) || ferror(out))
+			return EXIT_FAILURE;
+		read_bswap_write(in, out);
+	}
+
+	fclose(in);
+	fclose(out);
+
+	return EXIT_SUCCESS;
+}
